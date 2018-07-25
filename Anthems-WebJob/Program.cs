0bbbd2ac01ed74cef3b6f08044967476d5261601
Callum D E Smith <callumdesmith@callumdesmith.co.uk>
2018-07-25 15:37:42 +0100

    Modified Azure WebJob application code.

--- Anthems-WebJob/Program.cs
+++ Anthems-WebJob/Program.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 
 namespace Anthems_WebJob
 {
     // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
-    class Program
+    public class Program
     {
-        // Please set the following connection strings in app.config for this WebJob to run:
+        // Please set the following connection strings in app.config for this WebJob to run.
+        // These must be set with these names in WebJobs:-
         // AzureWebJobsDashboard and AzureWebJobsStorage
         static void Main()
         {
@@ -21,8 +18,15 @@ namespace Anthems_WebJob
                 config.UseDevelopmentSettings();
             }
 
-            var host = new JobHost();
-            // The following code ensures that the WebJob will be running continuously
+            // Default queue check has an exponentially increasing backoff interval.
+            // Optionally can set a cap on how long the polling interval will increase to.
+
+            config.Queues.MaxPollingInterval = TimeSpan.FromSeconds(5);
+
+            var host = new JobHost(config);
+
+            // The following code ensures that the WebJob will be running continuously.
+
             host.RunAndBlock();
         }
     }
